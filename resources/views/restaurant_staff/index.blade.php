948b7fbb0fa5f09cb33de739d1f08d743afb8a8f
anishregmi17 <anishregminaglibang@gmail.com>
2024-08-02 16:07:04 +0545

    update all

--- resources/views/restaurant_staff/index.blade.php
+++ resources/views/restaurant_staff/index.blade.php
@@ -4,34 +4,44 @@
     <div class="container">
         <h1 class="mt-4 mb-3">Our Staffs</h1>
         <a href="{{ route('restaurant-staff.create') }}" class="btn btn-primary mb-3">Create Staff</a>
-        <div class="row">
-            @foreach ($staff as $staffMember)
-                <div class="col-md-4 mb-4 d-flex align-items-stretch">
-                    <div class="card">
-                        @if ($staffMember->profile_image)
-                            <img src="{{ asset($staffMember->profile_image) }}"
-                                class="card-img-top rounded-circle mx-auto d-block" alt="{{ $staffMember->name }}">
-                        @else
-                            <img src="{{ asset('images/default-profile.png') }}"
-                                class="card-img-top rounded-circle mx-auto d-block" alt="Default Profile Image">
-                        @endif
-                        <div class="card-body text-center">
-                            <h6 class="card-title">{{ $staffMember->name }}</h6>
-                            <p class="card-text">{{ $staffMember->role }}-{{ $staffMember->contact }}</p>
-                            <div class="btn-group mt-auto">
-                                <a href="{{ route('restaurant-staff.edit', $staffMember->id) }}"
-                                    class="btn btn-warning">Edit</a>
-                                <form action="{{ route('restaurant-staff.destroy', $staffMember->id) }}" method="POST"
-                                    style="display:inline">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger">Delete</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        </div>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Profile Image</th>
+                    <th>Name</th>
+                    <th>Role</th>
+                    <th>Contact</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($staff as $staffMember)
+                    <tr>
+                        <td>
+                            @if ($staffMember->profile_image)
+                                <img src="{{ asset($staffMember->profile_image) }}" class="rounded-circle"
+                                    alt="{{ $staffMember->name }}" style="width: 50px; height: 50px;">
+                            @else
+                                <img src="{{ asset('images/default-profile.png') }}" class="rounded-circle"
+                                    alt="Default Profile Image" style="width: 50px; height: 50px;">
+                            @endif
+                        </td>
+                        <td>{{ $staffMember->name }}</td>
+                        <td>{{ $staffMember->role }}</td>
+                        <td>{{ $staffMember->contact }}</td>
+                        <td>
+                            <a href="{{ route('restaurant-staff.edit', $staffMember->id) }}"
+                                class="btn btn-warning btn-sm">Edit</a>
+                            <form action="{{ route('restaurant-staff.destroy', $staffMember->id) }}" method="POST"
+                                style="display:inline">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
